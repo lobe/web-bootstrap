3bdeb9e1ee35901a7aa1167581d038c936e918be
Erin Song <t-ersong@microsoft.com>
2020-08-03 10:26:59 -0700

    added responsiveness to webcam

--- src/Camera.js
+++ src/Camera.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import './index.css'
+import Webcam from "react-webcam";
+import './App.css'
 
-const CAPTURE_OPTIONS = {
-    audio: false,
-    video: { facingMode: "environment" }
-};
+function Camera() {
+    const videoConstraints = {
+        audio: false,
+        video: { facingMode: "user" },
+        width: 1280,
+        height: 1000
+    };
+
+    return (
+        <div>
+            <Webcam 
+            height = {100 + '%'}
+            width = {100 + '%'}
+            videoConstraints={videoConstraints}/>
+        </div>
+    )
+}
+
+export default Camera;
