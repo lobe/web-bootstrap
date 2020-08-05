20715ccb664dec7ca6d30b2f1359c0896747645f
Erin Song <erinsong@berkeley.edu>
2020-08-05 11:38:23 -0700

    Fullscreen webcam component (#2)
    * added responsiveness to webcam
    
    * resize camera to full screen
    
    * removed inline styles for webcam resize
    
    * added image capture method
    
    * updated capture function
    
    * modified camera css
    
    * fixed webcam styling
    
    Co-authored-by: Erin Song <t-ersong@microsoft.com>

--- src/Camera.js
+++ src/Camera.js
@@ -1,7 +1,37 @@
-import React from 'react';
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import Webcam from "react-webcam";
+import './App.css'
 
-const CAPTURE_OPTIONS = {
-    audio: false,
-    video: { facingMode: "environment" }
-};
+function Camera() {
+    const videoConstraints = {
+        audio: false,
+        video: { facingMode: "user" },
+    };
+
+    const webcamRef = React.useRef(null);
+    const [imageSrc, setImageSrc] = useState("default image");
+
+    const capture = React.useCallback(
+        () => {
+            if (webcamRef.current) {
+                setImageSrc(webcamRef.current.getScreenshot())
+                return imageSrc;
+            }
+        },
+        [webcamRef, imageSrc]
+    )
+
+    return (
+        <div className="camera" id="video-stream">
+            <Webcam
+                ref={webcamRef}
+                screenshotFormat = 'image/jpeg'
+                forceScreenshotSourceSize="true"
+                videoConstraints={videoConstraints}
+                style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center'  }}
+            />
+        </div>
+    )
+}
+
+export default Camera;
