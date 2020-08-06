986a7ae18eefc6818d7ce93c3e3d6296be06974c
Adam Menges <adam@adammenges.com>
2020-08-06 10:32:15 -0700

    Tensorflow.js and prediction component (#3)
    * added responsiveness to webcam
    
    * resize camera to full screen
    
    * removed inline styles for webcam resize
    
    * added image capture method
    
    * basic functionality
    
    * use the old camera for prediction but full screen camera for display..
    
    * import model url attempt
    
    * Create .nvmrc
    
    * Update yarn.lock
    
    * setup webpack and express
    
    * Added model
    
    * fixed model loading error
    
    * Update App.js
    
    * greenbar positioning, blurred box, separated modules
    
    * styling css
    
    * fix conflicts issues
    
    * clean up
    
    Co-authored-by: Erin Song <t-ersong@microsoft.com>
    Co-authored-by: gaoyuezhou <zhougy_99@berkeley.edu>
    Co-authored-by: gaoyuezhou <44328051+gaoyuezhou@users.noreply.github.com>

---
+++ src/Prediction.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as tmImage from "@teachablemachine/image";
+
+const URL = "http://localhost:3000/model/";
+
+function Prediction() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  var model = null;
+  var maxPredictions = null;
+  var webcam = null;
+  var labelContainer = null;
+
+  const [label, setLabel] = useState("default");
+  const [prob, setProb] = useState(0);
+
+
+  const loadModel = async () => {
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+
+    webcam = new tmImage.Webcam(1000, 800, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+  };
+
+  async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+  }
+
+  async function predict() {
+
+    const prediction = await model.predict(webcam.canvas);
+    var maxProb = 0;
+    var maxLabel = null;
+
+    for (let i = 0; i < maxPredictions; i++) {
+      if (prediction[i].probability > maxProb) {
+        maxProb = prediction[i].probability;
+        maxLabel = prediction[i].className;
+      }
+    }
+
+    setProb(maxProb * 100);
+    setLabel(maxLabel);
+  }
+
+  loadModel();
+
+  return (
+    <>
+<div class="blur">
+        <div
+          class="greenprogress"
+        >
+          <div
+            id="predlabel"
+            style={{
+              color: "#ffffff",
+              fontSize: "25px",
+              position: "absolute",
+              top: "16px",
+              left: "40px",
+            }}
+          >
+            {" "}
+            {label}{" "}
+          </div>
+          <div class="bar"  style={{
+              width: String(prob) + "%",
+              height: "40px"
+            }}></div>
+
+        </div>
+        </div>
+    </>
+  );
+}
+
+export default Prediction;
