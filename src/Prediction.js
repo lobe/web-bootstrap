14bfc0df29ba347a055f1cc508f3ced38d0460f2
gaoyuezhou <44328051+gaoyuezhou@users.noreply.github.com>
2020-08-06 20:26:48 -0700

    Kathy (#5)
    * added responsiveness to webcam
    
    * resize camera to full screen
    
    * removed inline styles for webcam resize
    
    * added image capture method
    
    * basic functionality
    
    * use the old camera for prediction but full screen camera for display..
    
    * import model url attempt
    
    * Create .nvmrc
    
    * Update yarn.lock
    
    * setup webpack and express
    
    * Added model
    
    * fixed model loading error
    
    * Update App.js
    
    * greenbar positioning, blurred box, separated modules
    
    * styling css
    
    * fix conflicts issues
    
    * clean up
    
    * fix loading model issue & reset frequency
    
    * bar animation
    
    * 3 preds
    
    * change the look
    
    Co-authored-by: Erin Song <t-ersong@microsoft.com>
    Co-authored-by: Adam Menges <adam@adammenges.com>

--- src/Prediction.js
+++ src/Prediction.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as tmImage from "@teachablemachine/image";
+import { transform } from "@babel/core";
 
 const URL = "http://localhost:3000/model/";
 
@@ -16,8 +17,21 @@ function Prediction() {
   const [label, setLabel] = useState("default");
   const [prob, setProb] = useState(0);
 
+  const [label2, setLabel2] = useState("default");
+  const [prob2, setProb2] = useState(0);
+
+  const [label3, setLabel3] = useState("default");
+  const [prob3, setProb3] = useState(0);
+
+  useEffect(() => {
+    // Your code here
+    loadModel()
+  }, []);
+
 
   const loadModel = async () => {
+    console.log("once")
+
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
     // Convenience function to setup a webcam
@@ -26,33 +40,51 @@ function Prediction() {
     webcam = new tmImage.Webcam(1000, 800, flip); // width, height, flip
     await webcam.setup(); // request access to the webcam
     await webcam.play();
+    // window.requestAnimationFrame(loop);
+    const interval = setInterval(() => {
+      loop()
+    }, 1000)
     window.requestAnimationFrame(loop);
   };
 
   async function loop() {
     webcam.update(); // update the webcam frame
     await predict();
-    window.requestAnimationFrame(loop);
   }
 
   async function predict() {
 
     const prediction = await model.predict(webcam.canvas);
-    var maxProb = 0;
-    var maxLabel = null;
+    var maxProb = [];
+    var maxLabel = [];
 
     for (let i = 0; i < maxPredictions; i++) {
-      if (prediction[i].probability > maxProb) {
-        maxProb = prediction[i].probability;
-        maxLabel = prediction[i].className;
+      if (maxProb.length < 3){
+        maxProb.push(prediction[i].probability)
+        maxLabel.push(prediction[i].className)
+      } else {
+        var indexMin = maxProb.indexOf(Math.min(...maxProb));
+        var minOf3 = maxProb[indexMin]
+        if (prediction[i].probability > minOf3) {
+          maxProb[indexMin] = prediction[i].probability;
+          maxLabel[indexMin] = prediction[i].className;
+        }
       }
     }
 
-    setProb(maxProb * 100);
-    setLabel(maxLabel);
-  }
+    var first = maxProb.indexOf(Math.max(...maxProb));
+    var third = maxProb.indexOf(Math.min(...maxProb));
+    var second = (first + third) * 2 % 3
 
-  loadModel();
+    setProb(maxProb[first] * 100);
+    setLabel(maxLabel[first]);
+
+    setProb2(maxProb[second] * 100);
+    setLabel2(maxLabel[second]);
+
+    setProb3(maxProb[third] * 100);
+    setLabel3(maxLabel[third]);
+  }
 
   return (
     <>
@@ -61,20 +93,61 @@ function Prediction() {
           class="greenprogress"
         >
           <div
-            id="predlabel"
+            class="predlabel"
             style={{
               color: "#ffffff",
               fontSize: "25px",
               position: "absolute",
-              top: "16px",
               left: "40px",
             }}
           >
-            {" "}
             {label}{" "}
           </div>
-          <div class="bar"  style={{
-              width: String(prob) + "%",
+          <div id="bar" class="bar"  style={{
+              width:String(prob) +"%",
+              height: "40px",
+              backgroundColor: "#00DDAD"
+            }}></div>
+
+        </div>
+
+        <div
+          class="greenprogress"
+        >
+          <div
+            class="predlabel"
+            style={{
+              color: "#ffffff",
+              fontSize: "25px",
+              position: "absolute",
+              left: "40px",
+            }}
+          >
+            {label2}{" "}
+          </div>
+          <div id="bar" class="bar"  style={{
+              width:String(prob2) +"%",
+              height: "40px"
+            }}></div>
+
+        </div>
+
+        <div
+          class="greenprogress"
+        >
+          <div
+            class="predlabel"
+            style={{
+              color: "#ffffff",
+              fontSize: "25px",
+              position: "absolute",
+              left: "40px",
+            }}
+          >
+            {label3}{" "}
+          </div>
+          <div id="bar" class="bar"  style={{
+              width:String(prob3) +"%",
               height: "40px"
             }}></div>
 
