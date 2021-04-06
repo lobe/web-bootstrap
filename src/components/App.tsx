83fd9b5ba2a15bad471752d7af16fc47ba85c536
Markus Beissinger <mbeissinger@gmail.com>
2021-04-06 11:34:33 -0700

    code cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState, useEffect} from 'react';
+import React, {useCallback, useState} from 'react';
 import Camera from './camera/Camera';
 import Prediction from './prediction/Prediction';
 import ImageSelectorButton from './staticImage/ImageSelectorButton';
